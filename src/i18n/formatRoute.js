50b420b5649acc6845823f9921350fc6dc07cfac
丁田秀 <295434665@qq.com>
2021-08-02 10:06:25 +0800

    处理routes.js文件

--- src/i18n/formatRoute.js
+++ src/i18n/formatRoute.js
@@ -4,7 +4,12 @@ const path = require('path')
 
 module.exports = (localeMap, prettierCode) => {
   const absolutePath = process.cwd()
-  const content = fs.readFileSync(path.join(absolutePath, '/config/routes.ts'), 'utf-8')
+  let content
+  if (fs.existsSync(path.join(absolutePath, '/config/routes.ts'))) {
+    content = fs.readFileSync(path.join(absolutePath, '/config/routes.ts'), 'utf-8')
+  } else if (fs.existsSync(path.join(absolutePath, '/config/routes.js'))) {
+    content = fs.readFileSync(path.join(absolutePath, '/config/routes.js'), 'utf-8')
+  } else return
   const formatContent = content.replace('export default', 'module.exports =')
   fs.writeFileSync('./routes.js', formatContent)
   const routes = require(path.join(absolutePath, '/routes.js'))
